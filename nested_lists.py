0c597af8fded0b0c74628c38250f4356b07cde9e
enriqueMota <abelmota@live.com>
2023-05-15 14:11:19 -0400

    nested lists

---
+++ nested_lists.py
@@ -0,0 +1,13 @@
+
+
+# states = ["Washington", "New York", "Delaware", "Pennsylvania"]
+
+# This will produce an index error because there is no value in that index
+# print(states[9])
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+veggies = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen = [fruits, veggies]
+
+print(dirty_dozen[1][1])
